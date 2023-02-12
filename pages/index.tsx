3b934b56e204bbe528c8300a4d4ca44ba1cf0c85
SahanDisa <tsahandisaai@gmail.com>
2023-02-12 18:05:02 +0530

    add 404 page

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,6 +4,7 @@ import { Box, Link, Checkbox, Container, FormControl, Button, InputLabel, MenuIt
 import Grid from '@mui/material/Unstable_Grid2';
 import { styled } from '@mui/material/styles';
 import { useState } from "react";
+import Head from "next/head";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -17,6 +18,7 @@ export default function Home() {
 
     return (
         <>
+            
             <div>
             <h1 className={styles.title}>Homepage</h1>
             <p className={styles.text}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus animi impedit suscipit architecto, odio inventore nostrum non neque dicta. Quam magni accusantium culpa distinctio tempore iure accusamus, dolorem nobis odit.</p>
