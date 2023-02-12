9e16eed0528d1178c7a4cf17d15f70512d5973e5
SahanDisa <tsahandisaai@gmail.com>
2023-02-12 15:35:47 +0530

    Update: styles + font style

--- pages/index.tsx
+++ pages/index.tsx
@@ -18,11 +18,11 @@ export default function Home() {
     return (
         <>
             <div>
-              <h1>Homepage</h1>
-              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus animi impedit suscipit architecto, odio inventore nostrum non neque dicta. Quam magni accusantium culpa distinctio tempore iure accusamus, dolorem nobis odit.</p>
-              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus animi impedit suscipit architecto, odio inventore nostrum non neque dicta. Quam magni accusantium culpa distinctio tempore iure accusamus, dolorem nobis odit.</p>
-              <Link href="/students/">
-                <a>See Student Listing</a>
+            <h1 className={styles.title}>Homepage</h1>
+            <p className={styles.text}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus animi impedit suscipit architecto, odio inventore nostrum non neque dicta. Quam magni accusantium culpa distinctio tempore iure accusamus, dolorem nobis odit.</p>
+            <p className={styles.text}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus animi impedit suscipit architecto, odio inventore nostrum non neque dicta. Quam magni accusantium culpa distinctio tempore iure accusamus, dolorem nobis odit.</p>
+            <Link href="/students/">
+                <a className={styles.btn}>See Student Listing</a>
               </Link>
             </div>
                 {/* <Box p={3}>
