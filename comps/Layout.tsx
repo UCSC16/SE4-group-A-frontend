1f14ef59c6625b59c873be29a89fb51757270651
Nandula Perera <35824376+nandulaperera@users.noreply.github.com>
2023-02-13 14:19:52 +0530

    Fix bug with import

--- comps/Layout.tsx
+++ comps/Layout.tsx
@@ -1,6 +1,6 @@
 import React, { FC } from "react";
 import Footer from "./Footer"
-import Navbar from "./NavBar";
+import Navbar from "./Navbar";
 
 type MyComponentProps = React.PropsWithChildren<{}>;
 
