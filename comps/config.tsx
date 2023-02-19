d049d9108083ea52a457108c9a3f0656e90928de
Nandula Perera <35824376+nandulaperera@users.noreply.github.com>
2023-02-19 19:05:23 +0530

    Update config.tsx

--- comps/config.tsx
+++ comps/config.tsx
@@ -1,6 +1,6 @@
 const backend =
   process.env.NODE_ENV === "production"
-    ? "10.5.0.7:80"
-    : "10.5.0.7:80";
+    ? "http://10.5.0.7:80"
+    : "http://10.5.0.7:80";
 
 export default backend;
